8c75743cda35cbea843cfe2aabeb9c337a5b9213
rotoyouoi <rotoyouoi@yandex.ru>
2023-12-21 09:28:57 +0500

    Добавьте файлы проекта.

---
+++ bebraProjectr777/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bebraProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] existingArray = { -2, -4, 0, 1, 2, 3, 4, 5, 9, 55 };
+            List<int> numbersList = new List<int>();
+
+            //  положительные элементы
+            foreach (int element in existingArray)
+            {
+                if (element % 2 == 0)
+                    numbersList.Add(element);
+            }
+
+            //  отрицательные элементы
+            foreach (int element in existingArray)
+            {
+                if (element % 2 == 1)
+                    numbersList.Add(element);
+            }
+
+            // Выводим все элементы 
+            foreach (int number in numbersList)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
